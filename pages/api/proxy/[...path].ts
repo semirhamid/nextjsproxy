976443a75ad582e0f7bfe6a00d9527a90d221314
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2024-08-04 13:18:14 +0300

    added proxy header

--- pages/api/proxy/[...path].ts
+++ pages/api/proxy/[...path].ts
@@ -1,6 +1,6 @@
 // pages/api/proxy/[...path].ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { createProxyMiddleware, RequestHandler } from 'http-proxy-middleware';
 
 export const config = {
   api: {
@@ -8,21 +8,44 @@ export const config = {
   },
 };
 
-const proxy = createProxyMiddleware({
+const proxy: RequestHandler = createProxyMiddleware({
   target: 'https://awashbank.com/exchange-historical/', // Replace with your target API
   changeOrigin: true,
   pathRewrite: {
     '^/api/proxy': '', // Remove `/api/proxy` prefix when forwarding
   },
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Host': 'awashbank.com',
+    'Pragma': 'no-cache',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36',
+    'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+  },
 });
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  return new Promise((resolve, reject) => {
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  return new Promise<void>((resolve, reject) => {
     proxy(req, res, (result) => {
       if (result instanceof Error) {
         return reject(result);
       }
-      resolve(result);
+      resolve();
     });
   });
 };
+
+export default handler;
