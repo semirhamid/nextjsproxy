1300a2f9d88ccc5af6f889c691d80240f3de1656
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2024-08-04 13:23:26 +0300

    add3ed header

--- pages/api/proxy/[...path].ts
+++ pages/api/proxy/[...path].ts
@@ -14,6 +14,7 @@ const proxy: RequestHandler = createProxyMiddleware({
   pathRewrite: {
     '^/api/proxy': '', // Remove `/api/proxy` prefix when forwarding
   },
+  logger: "console",
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
@@ -38,14 +39,36 @@ const proxy: RequestHandler = createProxyMiddleware({
 });
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  return new Promise<void>((resolve, reject) => {
-    proxy(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      resolve();
+  fetch('https://awashbank.com/exchange-historical/', {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'Accept-Encoding': 'gzip, deflate, br, zstd',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
+      'Host': 'awashbank.com',
+      'Pragma': 'no-cache',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'none',
+      'Sec-Fetch-User': '?1',
+      'Upgrade-Insecure-Requests': '1',
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36',
+      'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+      'sec-ch-ua-mobile': '?1',
+      'sec-ch-ua-platform': '"Android"',
+    }
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
     });
-  });
 };
 
 export default handler;
